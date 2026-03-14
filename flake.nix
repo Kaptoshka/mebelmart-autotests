04e21334e545fdb60ae27451f2caef55c8e593cf
Kaptoshka <kaptoshka@duck.com>
2026-03-15 02:09:52 +0400

    feat: add nodejs package for Playwright and webkit support

--- flake.nix
+++ flake.nix
@@ -10,6 +10,7 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+
         browsers = (builtins.fromJSON
           (builtins.readFile "${pkgs.playwright-driver}/browsers.json")).browsers;
 
@@ -18,6 +19,9 @@
 
         firefox-rev = (builtins.head
           (builtins.filter (x: x.name == "firefox") browsers)).revision;
+
+        webkit-rev = (builtins.head
+          (builtins.filter (x: x.name == "webkit") browsers)).revision;
       in
       {
         devShells.default = pkgs.mkShell {
@@ -31,20 +35,27 @@
             deadnix
             nixpkgs-fmt
 
+            nodejs
             playwright-driver.browsers
           ];
 
           shellHook = ''
             echo "Go GUI Autotest Framework"
             echo "  Go version: $(go version)"
-            echo "  Browser: chromium-${chromium-rev} (Nix store)"
-            echo "  Browser: firefox-${firefox-rev} (Nix store)"
+            echo "  Node version: $(node --version)"
+            echo "  Chromium rev: ${chromium-rev}"
+            echo "  Firefox rev: ${firefox-rev}"
+            echo "  WebKit rev: ${webkit-rev}"
 
             export PLAYWRIGHT_BROWSER_PATH=${pkgs.playwright-driver.browsers}
             export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
             export PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=1
+
+            export PLAYWRIGHT_NODEJS_PATH="${pkgs.nodejs}/bin/node"
+
             export PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH="${pkgs.playwright-driver.browsers}/chromium-${chromium-rev}/chrome-linux/chrome"
             export PLAYWRIGHT_FIREFOX_EXECUTABLE_PATH="${pkgs.playwright-driver.browsers}/firefox-${firefox-rev}/firefox/firefox"
+            export PLAYWRIGHT_WEBKIT_EXECUTABLE_PATH="${pkgs.playwright-driver.browsers}/webkit-${webkit-rev}/pw-run.sh"
           '';
         };
       }
