edf55ed11c407f0355e21e8624733371c583397c
Kaptoshka <kaptoshka@duck.com>
2026-03-15 02:29:14 +0400

    style(logger)!: remove global variables once, globalLogger

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -4,49 +4,37 @@ import (
 	"io"
 	"log/slog"
 	"os"
-	"sync"
 	"testing"
 	"time"
 )
 
-var (
-	globalLogger *slog.Logger
-	once         sync.Once
-)
+func init() {
+	level := os.Getenv("LOG_LEVEL")
 
-func initRoot(level string) {
-	once.Do(func() {
-		var lvl slog.Level
-		if err := lvl.UnmarshalText([]byte(level)); err != nil {
-			lvl = slog.LevelInfo
-		}
+	var lvl slog.Level
+	if err := lvl.UnmarshalText([]byte(level)); err != nil {
+		lvl = slog.LevelInfo
+	}
 
-		_ = os.MkdirAll("logs", os.ModePerm)
-		logFile, err := os.OpenFile(
-			"logs/session_"+time.Now().Format("2006-01-02_15-04-05")+".log",
-			os.O_CREATE|os.O_WRONLY, 0o666,
-		)
-		if err != nil {
-			panic(err)
-		}
+	_ = os.MkdirAll("logs", os.ModePerm)
 
-		handler := slog.NewTextHandler(
-			io.MultiWriter(
-				os.Stdout,
-				logFile,
-			),
+	logFile, err := os.OpenFile(
+		"logs/session_"+time.Now().Format("2006-01-02_15-04-05")+".log",
+		os.O_CREATE|os.O_WRONLY, 0o666,
+	)
 
-			&slog.HandlerOptions{
-				Level: lvl,
-			},
-		)
+	var output io.Writer
+	if err != nil {
+		output = os.Stdout
+	} else {
+		output = io.MultiWriter(os.Stdout, logFile)
+	}
 
-		globalLogger = slog.New(handler)
-		slog.SetDefault(globalLogger)
-	})
+	slog.SetDefault(slog.New(slog.NewTextHandler(output, &slog.HandlerOptions{
+		Level: lvl,
+	})))
 }
 
 func ForTest(t *testing.T) *slog.Logger {
-	initRoot("info")
-	return globalLogger.With("test", t.Name())
+	return slog.Default().With("test", t.Name())
 }
